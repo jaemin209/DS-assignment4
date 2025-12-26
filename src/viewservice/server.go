59a50ef4edb5bd16b7ead8fd7c9c0a938e31b7e7
jaemin209 <c20569@kma.ac.kr>
2025-12-26 16:45:05 +0900

    Update server.go

--- src/viewservice/server.go
+++ src/viewservice/server.go
@@ -43,6 +43,10 @@ func (vs *ViewServer) Ping(args *PingArgs, reply *PingReply) error {
 		vs.currView.Viewnum = 1
 		vs.primaryAckedCurrView = false
 	} else if args.Me == vs.currView.Primary && args.Viewnum == 0 {
+	} else if args.Me != vs.currView.Primary && args.Me != vs.currView.Backup == "" && vs.primaryAckedCurrView {
+		vs.idleServer = args.Me
+		vsx. currView.Viewnum++
+		vs.primaryAckedCurrView = fals
 	} else if args.Me != vs.currView.Primary && args.Me != vs.currView.Backup {
 		vs.idleServer = args.Me
 	}
